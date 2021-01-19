1a8a950760abc5a0ebe480db7b7c8a989f140c36
ale c. f <ale192837@live.com>
2021-01-19 14:06:24 -0300

    Adicionar arquivos de projeto.

---
+++ ACGun/Bullet.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ACGun
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Bullet : MonoBehaviour
+    {
+        public delegate void OnCollisionEnterDelegate(Vector3 p);
+
+        public event OnCollisionEnterDelegate OnCollisionEnterEventHandler;
+
+        public float updatePositionTimeRate = 0, damage;
+        /// <summary>
+        /// t, velocity
+        /// </summary>
+        public Func<float, float, Vector3> bulletTrajectory;
+        [HideInInspector]
+        public float velocity;
+        float t = 0;
+        Rigidbody2D rb;
+        public AudioSource audioSourcePrefab;
+        AudioSource audioSource;
+        public AudioClip bulletSound, collisionSound;
+        [HideInInspector]
+        public float rotationAngle;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            audioSource.clip = collisionSound;
+            audioSource.Play();
+
+            if (OnCollisionEnterEventHandler != null) OnCollisionEnterEventHandler(transform.position);
+            Destroy(gameObject);
+            //Damage calculator
+        }
+
+        Vector3 Rotate(Vector2 v)
+        {
+            float[][] rotationMatrix = new float[][] {
+            new float[] { Mathf.Cos(rotationAngle), Mathf.Sin(rotationAngle)},
+            new float[] { -Mathf.Sin(rotationAngle), Mathf.Cos(rotationAngle)}
+        };
+
+            float[] coordinatesBuffer = new float[] { 0, 0 };
+            float[] u = new float[] { v.x, v.y };
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    coordinatesBuffer[i] += rotationMatrix[i][j] * u[j];
+                }
+            }
+            return new Vector3(coordinatesBuffer[0], coordinatesBuffer[1], 0);
+        }
+
+        IEnumerator Wait(float waitTime)
+        {
+            rb.velocity = Rotate(bulletTrajectory(t, velocity));
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            audioSource = Instantiate(audioSourcePrefab);
+            audioSource.clip = bulletSound;
+            audioSource.Play();
+            Debug.Log(rotationAngle);
+            rotationAngle = Mathf.Deg2Rad * rotationAngle;
+
+        }
+
+        void FixedUpdate()
+        {
+            t += Time.deltaTime;
+            StartCoroutine(Wait(updatePositionTimeRate));
+        }
+    }
+
+}
---
+++ ACGun/Gun.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ACGun
+{
+    public class Gun : MonoBehaviour
+    {
+        public float damage, firingRatePerSecond, reloadGunTime;
+
+        public delegate void OnShoot();
+        public delegate void OnReloadGun();
+        public delegate void OnEmptyGunShoot();
+        public delegate void OnReloadedGun();
+        public delegate void OnCantShootCauseCondition();
+        public delegate void OnBulletCollisionEnterDelegate(Vector3 p);
+
+        public event OnBulletCollisionEnterDelegate OnBulletCollisionEnterEventHandler;
+        public event OnShoot OnShootEvenHandler;
+        public event OnReloadGun OnReloadGunEvenHandler;
+        public event OnEmptyGunShoot OnEmptyGunShootEventHandler;
+        public event OnReloadedGun OnReloadedGunEventHandler;
+        public event OnCantShootCauseCondition OnCantShootCauseConditionEventHandler;
+
+        public int ammo, maxAmmoInGun, maxAmmo, gunAmmo, decrementInShoot = 1;
+        public bool canShoot = true, gunLoad = true, infiniteAmmo;
+        public float bulletVelocity;
+        public AudioClip shootSound, bulletSound, bulletCollisionSound;
+        public AudioSource audioSource, bulletAudioSourcePrefab;
+        public GameObject bullet;
+        public Vector3 bulletInstantiatePoint;
+
+        // [HideInInspector]
+        public float rotationAngle = 0;
+
+        public Func<bool> conditionToShoot;
+        public Func<float> rotationAngleFunction;
+        /// <summary>
+        /// t, velocity
+        /// </summary>
+        public Func<float, float, Vector3> bulletTrajectory;
+
+        public void SetAudioSource(AudioClip audio) => audioSource.clip = audio;
+
+        public void Shoot()
+        {
+            bool fire = false;
+
+            if (gunAmmo > 0 || infiniteAmmo)
+            {
+                if (conditionToShoot != null)
+                {
+                    if (!conditionToShoot())
+                    {
+                        if (OnCantShootCauseConditionEventHandler != null)
+                            OnCantShootCauseConditionEventHandler();
+                    }
+                    fire = conditionToShoot();
+                }
+                else fire = true;
+            }
+            else if (!infiniteAmmo && gunAmmo == 0)
+            {
+                if (OnEmptyGunShootEventHandler != null) OnEmptyGunShootEventHandler();
+            }
+            if (fire && canShoot)
+            {
+                canShoot = false;
+                InstantiateBullet();
+                StartCoroutine(ReloadBullet(1f / firingRatePerSecond)); ;
+            }
+        }
+
+        IEnumerator ReloadBullet(float waitTime)
+        {
+            yield return new WaitForSeconds(waitTime);
+            canShoot = true;
+        }
+
+        IEnumerator ReloadGun(float waitTime)
+        {
+            yield return new WaitForSeconds(waitTime);
+            int ammoBuffer;
+            if (ammo > maxAmmoInGun) ammoBuffer = maxAmmoInGun;
+            else ammoBuffer = ammo;
+            gunAmmo = ammoBuffer;
+            ammo -= ammoBuffer;
+            canShoot = true;
+            if (OnReloadedGunEventHandler != null) OnReloadedGunEventHandler();
+        }
+
+        public void ReloadGun()
+        {
+            if (!infiniteAmmo)
+            {
+                if (ammo > 0)
+                {
+                    canShoot = false;
+                    if (OnReloadGunEvenHandler != null) OnReloadGunEvenHandler();
+                    StartCoroutine(ReloadGun(reloadGunTime)); ;
+
+                }
+            }
+        }
+        void InstantiateBullet()
+        {
+            GameObject bulletBuffer = Instantiate(bullet);
+            Bullet bulletComponent = bulletBuffer.GetComponent<Bullet>();
+            bulletComponent.transform.SetParent(gameObject.transform);
+            bulletBuffer.transform.localPosition = bulletInstantiatePoint;
+            bulletBuffer.transform.SetParent(null);
+
+            if (bulletTrajectory != null) bulletComponent.bulletTrajectory = bulletTrajectory;
+            bulletComponent.velocity = bulletVelocity;
+            bulletComponent.damage = damage;
+            bulletComponent.OnCollisionEnterEventHandler += (p) => {
+                if (OnBulletCollisionEnterEventHandler != null) OnBulletCollisionEnterEventHandler(p);
+            };
+            bulletComponent.bulletSound = bulletSound;
+            bulletComponent.collisionSound = bulletCollisionSound;
+            bulletComponent.audioSourcePrefab = bulletAudioSourcePrefab;
+            if (rotationAngleFunction != null)
+                bulletComponent.rotationAngle = rotationAngleFunction();
+            else
+                bulletComponent.rotationAngle = 0;
+
+            if (!infiniteAmmo) gunAmmo -= decrementInShoot;
+            if (OnShootEvenHandler != null) OnShootEvenHandler();
+        }
+
+        void Start()
+        {
+            if (shootSound != null)
+            {
+                audioSource.clip = shootSound;
+                OnShootEvenHandler += () => audioSource.Play();
+            }
+            Debug.Log("RotationAngle: " + rotationAngle);
+
+        }
+
+        void FixedUpdate()
+        {
+
+        }
+    }
+}
